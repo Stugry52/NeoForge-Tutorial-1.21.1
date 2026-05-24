992703a5e4d64f443d53e3553542a95fc1eb077a
Stugry52 <vladkravcenko95@gmail.com>
2026-05-24 14:29:57 +0300

    AddTools

--- src/main/java/net/stugry/tutorialmod/datagen/ModBlockTagProvider.java
+++ src/main/java/net/stugry/tutorialmod/datagen/ModBlockTagProvider.java
@@ -7,6 +7,7 @@ import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.stugry.tutorialmod.TutorialMod;
 import net.stugry.tutorialmod.block.ModBlocks;
+import net.stugry.tutorialmod.util.ModTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
@@ -21,13 +22,26 @@ public class ModBlockTagProvider extends BlockTagsProvider {
         tag(BlockTags.MINEABLE_WITH_PICKAXE)
                 .add(ModBlocks.BISMUTH_BLOCK.get())
                 .add(ModBlocks.BISMUTH_ORE.get())
+                .add(ModBlocks.BISMUTH_LAMP.get())
                 .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get())
                 .add(ModBlocks.MAGIC_BLOCK.get());
+
         tag(BlockTags.NEEDS_IRON_TOOL)
                 .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get());
+
+        tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.BISMUTH_LAMP.get());
 
         tag(BlockTags.FENCES).add(ModBlocks.BISMUTH_FENCE.get());
         tag(BlockTags.FENCE_GATES).add(ModBlocks.BISMUTH_FENCE_GATE.get());
         tag(BlockTags.WALLS).add(ModBlocks.BISMUTH_WALL.get());
+
+        tag(ModTags.Blocks.NEEDS_BISMUTH_TOOL)
+                .addTags(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.BISMUTH_LAMP.get());
+
+        tag(ModTags.Blocks.INCORRECT_FOR_BISMUTH_TOOL)
+                .addTags(BlockTags.INCORRECT_FOR_IRON_TOOL)
+                .remove(ModTags.Blocks.NEEDS_BISMUTH_TOOL);
     }
 }
