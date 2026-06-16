3822441f8b7663a6dbd617b74a0af751038baeab
Stugry52 <vladkravcenko95@gmail.com>
2026-06-17 00:13:37 +0300

    WitherOnlyNether2Change

--- src/main/java/net/stugry/tutorialmod/event/EncoreEvents.java
+++ src/main/java/net/stugry/tutorialmod/event/EncoreEvents.java
@@ -1,31 +1,46 @@
 package net.stugry.tutorialmod.event;
 
+import net.minecraft.client.model.WitherBossModel;
+import net.minecraft.client.renderer.entity.WitherBossRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.inventory.MerchantMenu;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.pattern.BlockInWorld;
+import net.minecraft.world.level.block.state.pattern.BlockPattern;
+import net.minecraft.world.level.block.state.pattern.BlockPatternBuilder;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.player.ItemEntityPickupEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.neoforge.event.village.VillagerTradesEvent;
 import net.stugry.tutorialmod.TutorialMod;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
 
 import static net.minecraft.world.Containers.dropItemStack;
 
@@ -156,32 +171,95 @@ public class EncoreEvents {
     }
 
     // Перехват события появления визера в мире
+//    @SubscribeEvent
+//    public static void onWitherEntityJoin(EntityJoinLevelEvent event){
+//        if (event.getEntity() instanceof WitherBoss witherBoss && !event.getLevel().isClientSide){
+//            Level level = event.getLevel();
+//
+//            // Отменяем призыв визера
+//            if (!level.dimension().equals(Level.NETHER)) {
+//
+//                BlockPos pos = witherBoss.blockPosition();
+//
+//                event.setCanceled(true);
+//
+//                // Выдаем, ввиде дропа, использованые предметы
+//                dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.WITHER_SKELETON_SKULL, 3));
+//
+//               BlockPos centerPos = pos.below();
+//               BlockState centerState = level.getBlockState(centerPos);
+//               ItemStack material = new ItemStack(Blocks.SOUL_SAND);
+//               if (centerState.is(Blocks.SOUL_SOIL)){
+//                   material = new ItemStack(Blocks.SOUL_SOIL);
+//               }
+//
+//               material.setCount(4);
+//                dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), material);
+//
+//                // Выводим текст игрокам вблизи призыва Визера
+//                level.players().stream()
+//                        .filter(p -> p.distanceToSqr(witherBoss.position()) < 25)
+//                        .forEach(p -> {
+//                            if (p instanceof ServerPlayer serverPlayer) {
+//                                serverPlayer.displayClientMessage(
+//                                        Component.literal("Ритуал не возможен вне Ада!"), true
+//                                );
+//                            }
+//                        });
+//
+//                level.playSound(null, pos, SoundEvents.WITHER_SPAWN, SoundSource.HOSTILE, 0.5f, 0.5f);
+//            }
+//        }
+//    }
+
+    private static final Map<BlockPos, List<BlockState>> STRUCTURE_CACHE = new ConcurrentHashMap<>();
+
     @SubscribeEvent
-    public static void onWitherEntityJoin(EntityJoinLevelEvent event){
-        if (event.getEntity() instanceof WitherBoss witherBoss && !event.getLevel().isClientSide){
+    public static void onInteract(PlayerInteractEvent.RightClickBlock event){
+        Level level = event.getLevel();
+        if (level.isClientSide() || !event.getItemStack().is(Items.WITHER_SKELETON_SKULL)) return;
+
+        BlockPos pos = event.getPos();
+        BlockPattern pattern = WitherStructure.getOrCreateWitherBase();
+        BlockPattern.BlockPatternMatch match = pattern.find(level, pos);
+
+        if (match != null){
+            List<BlockState> captured = new ArrayList<>();
+
+            for (int i = 0; i < pattern.getWidth(); i++){
+                for (int j = 0; j < pattern.getHeight(); j++){
+                    BlockState state = match.getBlock(i, j, 0).getState();
+                    if (state.is(BlockTags.WITHER_SUMMON_BASE_BLOCKS)){
+                        captured.add(state);
+                    }
+                }
+            }
+            STRUCTURE_CACHE.put(pos, captured);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onWitherSpawn(EntityJoinLevelEvent event){
+        if (event.getEntity() instanceof WitherBoss witherBoss && !event.getLevel().isClientSide()){
             Level level = event.getLevel();
+            if (level.dimension().equals(Level.NETHER)) return;
 
-            // Отменяем призыв визера
-            if (!level.dimension().equals(Level.NETHER)) {
+            BlockPos spawnPos = witherBoss.blockPosition();
 
-                BlockPos pos = witherBoss.blockPosition();
+            BlockPos key = STRUCTURE_CACHE.keySet().stream()
+                    .filter(pos -> pos.distSqr(spawnPos) < 9)
+                    .findFirst().orElse(null);
 
+            if (key != null){
                 event.setCanceled(true);
+                List<BlockState> materials = STRUCTURE_CACHE.remove(key);
 
-                // Выдаем, ввиде дропа, использованые предметы
-                dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.WITHER_SKELETON_SKULL, 3));
+                for (BlockState state : materials){
+                    level.addFreshEntity(new ItemEntity(level, spawnPos.getX() + 0.5, spawnPos.getY() + 0.5, spawnPos.getZ() + 0.5, new ItemStack(state.getBlock())));
+                }
 
-               BlockPos centerPos = pos.below();
-               BlockState centerState = level.getBlockState(centerPos);
-               ItemStack material = new ItemStack(Blocks.SOUL_SAND);
-               if (centerState.is(Blocks.SOUL_SOIL)){
-                   material = new ItemStack(Blocks.SOUL_SOIL);
-               }
+                level.addFreshEntity(new ItemEntity(level, spawnPos.getX() + 0.5, spawnPos.getY() + 1.0, spawnPos.getZ() + 0.5, new ItemStack(Items.WITHER_SKELETON_SKULL)));
 
-               material.setCount(4);
-                dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), material);
-
-                // Выводим текст игрокам вблизи призыва Визера
                 level.players().stream()
                         .filter(p -> p.distanceToSqr(witherBoss.position()) < 25)
                         .forEach(p -> {
@@ -192,9 +270,31 @@ public class EncoreEvents {
                             }
                         });
 
-                level.playSound(null, pos, SoundEvents.WITHER_SPAWN, SoundSource.HOSTILE, 0.5f, 0.5f);
+                level.playSound(null, spawnPos, SoundEvents.WITHER_SPAWN, SoundSource.HOSTILE, 0.5f, 0.5f);
             }
         }
     }
+
+    private static class WitherStructure{
+        private static BlockPattern witherBasePattern;
+
+        private static final Predicate<BlockInWorld> IS_SOUL_BLOCK = blockInWorld -> {
+            BlockState state = blockInWorld.getState();
+            return state.is(Blocks.SOUL_SAND) || state.is(Blocks.SOUL_SOIL);
+        };
+
+        public static BlockPattern getOrCreateWitherBase(){
+            if (witherBasePattern == null){
+                witherBasePattern = BlockPatternBuilder.start()
+                        .aisle("^^^", "###", "~#~")
+                        .where('#', IS_SOUL_BLOCK)
+                        .where('^', BlockInWorld.hasState(s -> s.is(Blocks.WITHER_SKELETON_SKULL) || s.is(Blocks.WITHER_SKELETON_WALL_SKULL)))
+                        .where('~', BlockInWorld.hasState(s -> s.is(Blocks.AIR)))
+                        .build();
+            }
+            return witherBasePattern;
+        }
+    }
+
 
 }
